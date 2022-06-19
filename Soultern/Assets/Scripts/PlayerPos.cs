ca4aa2ea24b4f6bb9d111c5d8643f80ca7ea3d56
PDMatsaliev20 <PDMatsaliev20@codingburgas.bg>
2022-06-19 18:19:25 +0300

    Add checkpoints (fix #54)

---
+++ Soultern/Assets/Scripts/PlayerPos.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerPos : MonoBehaviour
+{
+    private GameMaster gm;
+
+    void Start()
+    {
+        gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        transform.position = gm.lastCheckPointPos;
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
