eaf5d889550c20f1a8ee3a47eb7242e55195ac44
NTIvanova21 <NTIvanova21@codingburgas.bg>
2022-06-24 23:20:42 +0300

    Add nav bar and make changes to the main page

--- Website/js/main.js
+++ Website/js/main.js
@@ -244,27 +244,24 @@
 
 
 //Get the button
-let mybutton = document.getElementById("btn-back-to-top");
-
+var mybutton = document.getElementById("back-to-top");
+  
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
+window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (
-    document.body.scrollTop > 20 ||
-    document.documentElement.scrollTop > 20
-  ) {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
   }
 }
-// When the user clicks on the button, scroll to the top of the document
-mybutton.addEventListener("click", backToTop);
 
-function backToTop() {
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+
+
