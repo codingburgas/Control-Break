73f13401fa0329165c85ab544864a5c37fa96af3
NTIvanova21 <NTIvanova21@codingburgas.bg>
2022-06-26 23:37:49 +0300

    Add trailer for the game

--- Website/js/main.js
+++ Website/js/main.js
@@ -239,28 +239,8 @@
   });
 
 })()
-/*
-var mybutton = document.getElementById("top-button");
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {
-  scrollFunction()
-};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-  document.body.scrollTop = 0;
-  document.documentElement.scrollTop = 0;
-}
-*/
 
 
 
